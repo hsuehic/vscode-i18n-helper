ccd909a291d5ea56ba5efe9708df789e4f6c80e1
Xue Xiaowei <xiaowei.xue@xiaoweixue-mac.local>
2018-10-10 18:00:13 +0800

    docs: added comment.

--- src/getDictionary.ts
+++ src/getDictionary.ts
@@ -4,6 +4,9 @@ import * as fs from 'fs';
 import { getConfig } from './config';
 import { KEY_INTL_DICTIONARY_PATH } from './constant';
 
+/**
+ * Get and update i18n directory
+ */
 export const getDictionary = (): object => {
   const dictionaryPath = getConfig(KEY_INTL_DICTIONARY_PATH);
   const fullDictionaryPath = path.resolve(
@@ -15,7 +18,5 @@ export const getDictionary = (): object => {
     delete require.cache[require.resolve(fullDictionaryPath)];
     v = require(fullDictionaryPath);
   }
-  console.log('Dictionary:');
-  console.log(JSON.stringify(v));
   return v;
 };
