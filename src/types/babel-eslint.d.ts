3bfd0b4c9f9bd8bd917a19cadcd0ae91dd840185
xiaowei.xue <xiaowei.xue@shopee.com>
2018-10-11 21:10:33 +0800

    feat: type declaration  for babel-eslint.

---
+++ src/types/babel-eslint.d.ts
@@ -0,0 +1,38 @@
+// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: [~THE PROJECT NAME~]
+// Definitions by: [~YOUR NAME~] <[~A URL FOR YOU~]>
+
+/*~ This is the module plugin template file. You should rename it to index.d.ts
+ *~ and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+
+/*~ On this line, import the module which this module adds to */
+import * as eslint from 'eslint';
+
+/*~ Here, declare the same module as the one you imported above */
+declare module 'babel-eslint' {
+  export interface ParseOptions {
+    ecmaVersion?: 3 | 5 | 6 | 7 | 8 | 9 | 10 | 2015 | 2016 | 2017 | 2018 | 2019;
+    sourceType?: 'script' | 'module';
+    allowImportExportEverywhere?: boolean;
+  }
+
+  export function parse(
+    code: string,
+    options: ParseOptions
+  ): eslint.AST.Program;
+
+  export function parseForESLint(
+    code: string,
+    options: ParseOptions
+  ): eslint.AST.Program;
+
+  export function parseNoPatch(
+    code: string,
+    options: ParseOptions
+  ): eslint.AST.Program;
+
+  export default {};
+}
